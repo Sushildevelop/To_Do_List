73df7965be022b6272f49314a79ae1741c45b140
Sushil <chaubeyshushil000@gmail.com>
2023-10-01 11:25:30 +0530

    To-Do-List

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const utem = document.querySelector("#item")
+const toDoBox = document.querySelector("#to-do-box");
+
+item.addEventListener("keyup",
+    function(event) {
+        if (event.key === "Enter") {
+            addToDo(this.value)
+            this.value = ""
+        }
+
+    }
+
+)
+
+const addToDo = (item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+    ${item}
+    <i class='bx bx-message-alt-x'></i>`;
+
+    listItem.addEventListener("click",
+        function() {
+            this.classList.toggle("done");
+        })
+    listItem.querySelector("i").addEventListener("click",
+        function() {
+            listItem.remove();
+        })
+    toDoBox.appendChild(listItem)
+}
